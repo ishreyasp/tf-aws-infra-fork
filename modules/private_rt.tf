6ec33ea88a2f1412b2c9e4461d06d5a954ece63b
ishreyasp <purkarshreyas@gmail.com>
2025-02-17 15:00:16 -0500

    Add create public and private route tables script

---
+++ modules/private_rt.tf
@@ -0,0 +1,15 @@
+# Create a Private Route Table
+resource "aws_route_table" "csye6225_private_rt" {
+    vpc_id = aws_vpc.csye6225_vpc.id
+
+    tags = {
+        Name = "${var.environment}-csye6225-private-rtb"
+    }
+}
+
+# Private Route Table Associations
+resource "aws_route_table_association" "csye6225_private_rt_association" {
+    count          = var.private_subnet_count
+    subnet_id      = aws_subnet.csye6225_private_subnet[count.index].id
+    route_table_id = aws_route_table.csye6225_private_rt.id
+}
